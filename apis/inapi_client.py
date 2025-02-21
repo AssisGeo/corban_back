669dbf92ce509d3ca4b2ad6a7b277ac58cf43426
Renato Mendes <mendeslrenato@gmail.com>
2025-02-21 18:14:59 -0300

    feat: add redis in_100 cache

--- apis/inapi_client.py
+++ apis/inapi_client.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from services.inapi.redis_cache import get_in100_from_cache, add_in100_to_cache
 
 
 class InApiClient:
@@ -7,6 +8,10 @@ class InApiClient:
         self.base_url = "https://inapi.digital/api/check/consult"
 
     def get_in_100(self, cpf: str, benefit: str):
+        redis_key = f"in100_{cpf}_{benefit}"
+        cached_in100 = get_in100_from_cache(redis_key)
+        if cached_in100 is not None:
+            return cached_in100
         in_api_token = os.getenv("INAPI_TOKEN")
         headers = {
             "Accept": "application/json",
@@ -27,6 +32,7 @@ class InApiClient:
             json = response.json()
 
             if json["error"] is False:
+                add_in100_to_cache(redis_key, json["payload"])
                 return json["payload"]
             else:
                 return None
